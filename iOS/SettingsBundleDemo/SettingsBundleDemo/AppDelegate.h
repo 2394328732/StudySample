0712b3f4d440ed7b14d6c664d292c932ffdfab3e
yhzhou <gosailing@outlook.com>
2015-05-06 20:27:58 +0800

    init Sample code repo

---
+++ iOS/SettingsBundleDemo/SettingsBundleDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SettingsBundleDemo
+//
+//  Created by xiaoxin on 15/5/6.
+//  Copyright (c) 2015年 sailing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/SettingsBundleDemo/SettingsBundleDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SettingsBundleDemo
+//
+//  Created by xiaoxin on 15/5/6.
+//  Copyright (c) 2015年 sailing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
